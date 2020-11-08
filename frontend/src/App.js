cea2c92ac8b0e7bba00fa6f2f052f71237cdb922
bea94k <beatakusnierz94@gmail.com>
2020-11-08 20:19:50 +0200

    Add background blob

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,21 @@ import Main from "./components/Main/Main";
 import NotFound from "./components/NotFound/NotFound";
 import Footer from "./components/Footer/Footer";
 
+import blob1 from "./assets/blob1.svg";
+
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
       <div className="app">
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route path="/notfound" component={NotFound} />
-        </Switch>
+        <img src={blob1} alt="blob 1" className="blob blob1" />
+        <div className="content-wrap">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path="/notfound" component={NotFound} />
+          </Switch>
+        </div>
         <Footer />
       </div>
     </Router>

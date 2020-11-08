db9d78fbea848142c5b0c20a5c90a9901507f693
bea94k <beatakusnierz94@gmail.com>
2020-11-08 13:26:35 +0200

    Hitting enter works like clicking the submitting button

--- frontend/src/components/Input/Input.js
+++ frontend/src/components/Input/Input.js
@@ -4,12 +4,20 @@ import Button from "../Button/Button";
 import "./Input.css";
 
 const Input = (props) => {
+  // hitting Enter key triggers same action as clicking the button
+  const handleKeyUp = (event) => {
+    if (event.key === "Enter") {
+      props.triggerPostingUrl();
+    }
+  };
+
   return (
     <div className="input-wrapper">
       <input
         type="text"
         onChange={props.watchInputChange}
         onFocus={props.clearOnFocus}
+        onKeyUp={handleKeyUp}
       ></input>
       <Button text="Make it short!" onClick={props.triggerPostingUrl} />
     </div>
